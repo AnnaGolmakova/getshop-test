971acd58718e9028ef331691ebb5106c26854c96
Anna Golmakova <ann.golmakova@gmail.com>
2024-09-29 14:44:22 +0100

    created Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,8 +7,12 @@ export default function Header() {
       <div className={styles["header__logo"]}>Logo</div>
       <nav className={styles["header__menu"]}>
         <ul className={styles["header__menu-list"]}>
-          <li className={styles["header__menu-item"]}>Как работаем</li>
-          <li className={styles["header__menu-item"]}>Преимущества</li>
+          <li className={styles["header__menu-item"]}>
+            <a href="#balance">Преимущества</a>
+          </li>
+          <li className={styles["header__menu-item"]}>
+            <a href="#benefits">Как работаем</a>
+          </li>
         </ul>
       </nav>
     </header>
